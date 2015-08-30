c674b73cc0c5faaa125a2689d7dca323612ff9be
Peetz0r <peter@haas-en-berg.nl>
2015-08-31 01:14:18 +0200

    Add somewhat useful sketch using all of those libraries

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,7 +1,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <Bounce2.h>
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(5, 0, 3);
+Adafruit_PCD8544 d = Adafruit_PCD8544(5, 0, 3);
+Bounce b_u = Bounce();
+Bounce b_d = Bounce();
+Bounce b_l = Bounce();
+Bounce b_r = Bounce();
+Bounce b_s = Bounce();
 
 // these pin numbers are for the Pro Mini breadboard version
 // the Nano PCB version has different numbers
@@ -11,21 +17,63 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(5, 0, 3);
 #define PIN_U 4
 #define PIN_S 9
 
+int selected = 2;
+
 void setup()
 {
-	pinMode(DPAD_L, INPUT_PULLUP);
-	pinMode(DPAD_R, INPUT_PULLUP);
-	pinMode(DPAD_D, INPUT_PULLUP);
-	pinMode(DPAD_U, INPUT_PULLUP);
-	pinMode(DPAD_S, INPUT_PULLUP);
+	pinMode(PIN_L, INPUT_PULLUP);
+	pinMode(PIN_R, INPUT_PULLUP);
+	pinMode(PIN_D, INPUT_PULLUP);
+	pinMode(PIN_U, INPUT_PULLUP);
+	pinMode(PIN_S, INPUT_PULLUP);
 
-	display.begin();
-	display.setContrast(55);
-	display.clearDisplay();
+	b_u.attach(PIN_U);
+	b_d.attach(PIN_D);
+	b_l.attach(PIN_L);
+	b_r.attach(PIN_R);
+	b_s.attach(PIN_S);
+
+	b_u.interval(50);
+	b_d.interval(50);
+	b_l.interval(50);
+	b_r.interval(50);
+	b_s.interval(50);
+
+	d.begin();
+	d.setContrast(55);
 }
 
 
 void loop()
 {
+	b_u.update();
+	b_d.update();
+	b_l.update();
+	b_r.update();
+	b_s.update();
 
+	if(b_u.fell()) selected--;
+	if(b_d.fell()) selected++;
+	if(selected > 4) selected = 0;
+	if(selected < 0) selected = 4;
+
+	d.clearDisplay();
+	d.drawRoundRect(0,0, 84,48, 10, 1);
+
+	d.drawRoundRect(4,selected*9, 76,11, 5, 1);
+	d.drawLine(5,selected*9+5, 10,selected*9+5, 1);
+	d.fillTriangle(10,selected*9+3, 10,selected*9+7, 12,selected*9+5, 1);
+
+	d.setCursor(16, 2);
+	d.print("fqgjABC 1");
+	d.setCursor(16, 11);
+	d.print("fqgjABC 2");
+	d.setCursor(16, 20);
+	d.print("fqgjABC 3");
+	d.setCursor(16, 29);
+	d.print("fqgjABC 4");
+	d.setCursor(16, 38);
+	d.print("fqgjABC 5");
+
+	d.display();
 }
