0473be390b3c93a902dc238852c2ead201386454
Peetz0r <peter@haas-en-berg.nl>
2016-05-05 19:03:52 +0200

    believe it or not, this saves 1084 bytes of arduino space

--- src/sketch.ino
+++ src/sketch.ino
@@ -425,7 +425,7 @@ void tetris_draw(int m_x, int m_y) {
 								Serial.println(i);
 
 								for(int j = 0; j < 10; j++) {
-									uint16_t mask_r = pow(2, i+1)-1;
+									uint16_t mask_r = (1 << i+1)-1;
 									uint16_t mask_l = ~mask_r;
 									tetris_state[j] = (tetris_state[j]&mask_l) | (tetris_state[j]&mask_r>>1) << 1;
 								}
